386755fa363ccfa244483ee3a0f81794c311b534
VeryHandsomeMan55 <brodsky0562@gmail.com>
2019-11-19 17:34:54 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Michael Brodsky Peroid:2</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      
+      <li><a href="Brodsky_AboutMe.html"> AboutMe.html</a></li></br>
+      <li><a href="Brodsky_Projects.html"> Projects.html</a></li></br>
+      <li><a href="Brodsky_Resume.html"> Resume.html</a></li></br>
+      <li><a href="Brodsky_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
